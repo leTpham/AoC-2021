391dfbab24a294dbacb90cf7a5993e86c4926df3
Le Pham <phamtle724@gmail.com>
2022-10-26 18:59:52 -0700

    giant squid part 1 done

--- day4/giantSquidPart2.js
+++ day4/giantSquidPart2.js
@@ -1,13 +1,13 @@
+
 "use strict";
 
 const { readFile, getLinesFromString } = require('../handleInput');
-
 
 /** get input file as one array, first element is the numbers that are called,
  * every new element is one new line on a board -> 5 next elements make up a board
  */
 async function getInput() {
-  const input = await readFile("giantSquidInput.txt");
+  const input = await readFile("inputExample.txt");
   let result = getLinesFromString(input);
   return result;
 }
@@ -29,8 +29,8 @@ async function winBoard() {
   }
 
   //iterate through each random chosen number
-  for (let r = 0; r < chosenNums.length; r++) {
 
+  for (let r = 0; r < chosenNums.length; r++) {
     //iterate through each board
     for (let b = 0; b < boards.length; b++) {
 
